3ee4673199852268ee054886a85b8212907749d0
Robert Rumney <robertrumney@gmail.com>
2024-10-08 15:40:34 +0200

    Update UnusedTagCleaner.cs

--- UnusedTagCleaner.cs
+++ UnusedTagCleaner.cs
@@ -3,18 +3,20 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 
+using System.IO;
 using System.Linq;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public class UnusedTagCleaner : EditorWindow
 {
     private List<string> unusedTags = new List<string>();
     private bool scanCompleted = false;
 
-    [MenuItem("Tools/Unused Tags Remover")]
+    [MenuItem("Tools/Unused Tag Cleaner")]
     public static void ShowWindow()
     {
-        GetWindow<UnusedTagCleaner>("Unused Tag Cleaner");
+        GetWindow<UnusedTagCleaner>("Unused Tags Remover");
     }
 
     private void OnGUI()
@@ -44,12 +46,26 @@ public class UnusedTagCleaner : EditorWindow
         var allTags = new List<string>(UnityEditorInternal.InternalEditorUtility.tags);
         var usedTags = new HashSet<string>();
 
+        // Scan all scenes
         foreach (var scenePath in EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(scene => scene.path))
         {
             var scene = EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
             foreach (var go in scene.GetRootGameObjects())
             {
                 GetUsedTags(go, usedTags);
+            }
+        }
+
+        // Scan all scripts for dynamic tag references
+        var scriptFiles = Directory.GetFiles(Application.dataPath, "*.cs", SearchOption.AllDirectories);
+        var tagRegex = new Regex(@"\btag\b\s*=\s*""([^""]+)""");
+        foreach (var file in scriptFiles)
+        {
+            string content = File.ReadAllText(file);
+            var matches = tagRegex.Matches(content);
+            foreach (Match match in matches)
+            {
+                usedTags.Add(match.Groups[1].Value);
             }
         }
 
